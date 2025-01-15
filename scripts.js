a3f754b49238a41a3dcd38c417373ee91d4f5027
Adriyelle <152693509+Adriyelle@users.noreply.github.com>
2025-01-15 20:46:15 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,3 @@
-// JavaScript for Navigation
-function navigate(sectionId) {
-    // Hide all sections
-    const sections = document.querySelectorAll('.content');
-    sections.forEach((section) => (section.style.display = 'none'));
-
-    // Show the selected section
-    document.getElementById(sectionId).style.display = 'block';
-}
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Website Loaded Successfully!");
+});
